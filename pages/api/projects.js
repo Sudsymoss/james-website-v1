2bd614e6cebd456eb8d4e827e5221552b327f041
Sudsymoss <103403655+Sudsymoss@users.noreply.github.com>
2022-08-18 09:41:46 +1200

    projects

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json([{"id": "https://old.suddsy.dev", "name": "Old Suddsy.dev"},{"id": "projects/schoolsite", "name": "School site"},{"id": "projects/web4", "name": "Website 4.0"},{"id": "projects/jamessite", "name": "James Site"}],)
+  res.status(200).json([{"id": "https://old.suddsy.dev", "name": "Old Suddsy.dev"},{"id": "projects/schoolsite", "name": "School site"},{"id": "projects/web4", "name": "Website 4.0"},{"id": "projects/jamessite", "name": "James Site"},{"id": "https://suddsy.dev", "name":"Suddsy.dev"}],)
 }
