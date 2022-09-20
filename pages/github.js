1628eccae2b7b414a8e015464918c1b6dc0ddd8e
Sudsymoss <103403655+Sudsymoss@users.noreply.github.com>
2022-09-21 10:30:58 +1200

    github

---
+++ pages/github.js
@@ -0,0 +1,9 @@
+export default function github(){
+    return(
+        <div>
+            <script>
+                window.location.replace('https://github.com/Sudsymoss/james-website');
+            </script>
+        </div>
+    )
+}
