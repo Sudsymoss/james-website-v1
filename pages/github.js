56616cc39bb2f12e6f0785a7a7a8ba14762f822c
Sudsymoss <103403655+Sudsymoss@users.noreply.github.com>
2022-09-21 10:33:05 +1200

    github

--- pages/github.js
+++ pages/github.js
@@ -1,6 +1,10 @@
+import Head from 'next/head'
 export default function github(){
     return(
         <div>
+            <Head>
+                <meta http-equiv="refresh" content="2;url=https://github.com"/>
+            </Head>
             <script>
                 window.location.replace('https://github.com/Sudsymoss/james-website');
             </script>
