6729073d80cfc3e60a641f3184a087446a0f48d8
Sudsymoss <103403655+Sudsymoss@users.noreply.github.com>
2022-10-20 12:34:45 +1300

    loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import { SessionProvider } from "next-auth/react"
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Loader from '../component/loader';
+import { Suspense } from 'react'
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Suspense fallback={Loader}>
+        <Component {...pageProps} />
+      </Suspense>
     </SessionProvider>
   )
 }
