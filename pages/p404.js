373cd5cdaa0c257253326eceb39a4f73efaa862e
Sudsymoss <james1mowat@gmail.com>
2022-11-02 17:32:06 +1300

    projects

--- pages/p404.js
+++ pages/p404.js
@@ -13,7 +13,7 @@ export default function Home({ data }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Projects</title>
+        <title>404</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -29,39 +29,23 @@ export default function Home({ data }) {
       </Head>
       <Nav />
       <main className={styles.main}>
-        <h1>Projects</h1>
-      </main>
-      <section className={styles.projects}>
-        <div className={styles.gridbody}>
-          <div className={styles.gridcontainer}>
-            {projects.map((item) => (
-              <>
-                <div className={styles.gridform}>
-                  <Link href="/p404" key={item.id}>
-                    <div className={styles.griditem} key={item.id}>
-                      {item.title}
-                    </div>
-                  </Link>
-                  <p>Last Updated: {item.updatedAt || "null"}</p>
-                </div>
-              </>
-            ))}
-          </div>
+        <div className={styles.p404}>
+          <h1>Sorry!</h1>
+          <h3>Project currently unavailable for view!</h3>
+          <Link passHref href="/projects"><a className={styles.homebut}>back</a></Link>
         </div>
-      </section>
+      </main>
       <Footer />
     </div>
   );
 }
 
-//href for plink is {item.name}
-
 export async function getServerSideProps({ req, res }) {
   const projects = await prisma.project.findMany();
   res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=119, stale-while-revalidate=299'
-  )
+    "Cache-Control",
+    "public, s-maxage=119, stale-while-revalidate=299"
+  );
   return {
     props: {
       data: JSON.parse(JSON.stringify(projects)),
